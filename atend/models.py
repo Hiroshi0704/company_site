f10b236ae1bdcace0f951f9e7fe3dff02c3959ef
渡邉　博 <hiroshi@MacBook.local>
2019-09-03 07:51:38 +0900

    first commit

---
+++ atend/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Travelex(models.Model):
+
+    date  = models.DateField()
+    start_place = models.CharField(max_length=255)
+    end_place   = models.CharField(max_length=255)
+    value  = models.IntegerField()
+    reason = models.CharField(max_length=255)
+
+    def __str__(self):
+        return  self.date + '' + self.start_place + '~' + self.end_place + ': ' + self.value
+
+
+class Salary(models.Model):
+
+    date = models.DateField()
