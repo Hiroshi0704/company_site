47dc2d0172e0e4f0ee4cd5be6fa002cf732ba741
macair2 elevenlab <macair2elevenlab@macair2noMacBook-Pro.local>
2019-09-03 09:18:01 +0900

    add home.html

--- atend/urls.py
+++ atend/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from .views import HomePageView
+
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('atend/salary/list/', SalaryListView.as_view(), name='salary_list'),
-    path('atend/travelex/create/', TravelexCreateView.as_view(), name='travelex_create'),
-    path('atend/travelex/list/', TravelexListView.as_view(), name='travelex_list'),
-    path('atend/worklog/create/', WorklogCreateView.as_view(), name='worklog_create'),
+    # path('salary/list/', SalaryListView.as_view(), name='salary_list'),
+    # path('travelex/create/', TravelexCreateView.as_view(), name='travelex_create'),
+    # path('travelex/list/', TravelexListView.as_view(), name='travelex_list'),
+    # path('worklog/create/', WorklogCreateView.as_view(), name='worklog_create'),
 ]
