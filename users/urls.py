a1736607b6a341ef1ee822a6827850d2ab0cfcd8
macair2 elevenlab <macair2elevenlab@macair2noMacBook-Pro.local>
2019-09-03 09:00:48 +0900

    first migtate

---
+++ users/urls.py
@@ -0,0 +1,14 @@
+
+
+
+
+# --- users url path list --- #
+
+# users/ login/ [name='login']
+# users/ logout/ [name='logout']
+# users/ password_change/ [name='password_change']
+# users/ password_change/done/ [name='password_change_done']
+# users/ password_reset/ [name='password_reset']
+# users/ password_reset/done/ [name='password_reset_done']
+# users/ reset/<uidb64>/<token>/ [name='password_reset_confirm']
+# users/ reset/done/ [name='password_reset_complete']
