35f22b5982045bf699e75d20b7524c19d267fffd
macair2 elevenlab <macair2elevenlab@macair2noMacBook-Pro.local>
2019-09-03 09:36:30 +0900

    add work log

--- atend/urls.py
+++ atend/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from .views import HomePageView, SalaryListView, TravelexCreateView
+from .views import (
+    HomePageView, 
+    SalaryListView, 
+    TravelexCreateView, TravelexListView, 
+    WorklogCreateView,
+)
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('salary/list/', SalaryListView.as_view(), name='salary_list'),
     path('travelex/create/', TravelexCreateView.as_view(), name='travelex_create'),
-    # path('travelex/list/', TravelexListView.as_view(), name='travelex_list'),
-    # path('worklog/create/', WorklogCreateView.as_view(), name='worklog_create'),
+    path('travelex/list/', TravelexListView.as_view(), name='travelex_list'),
+    path('worklog/create/', WorklogCreateView.as_view(), name='worklog_create'),
 ]
--- atend/views.py
+++ atend/views.py
@@ -4,14 +4,36 @@ from django.views.generic.edit import CreateView
 
 from .models import Salary, Worklog, Travelex
 
+
+
+# MARK: - Home
+
 class HomePageView(TemplateView):
     template_name = 'atend/home.html'
 
 
+
+# MARK: - Salary
+
 class SalaryListView(ListView):
     model = Salary
 
+
+
+# MARK: - Traveling Expenses
+
 class TravelexCreateView(CreateView):
     model = Travelex
+    fields = '__all__'
+
+class TravelexListView(ListView):
+    model = Travelex
+
+
+
+# MARK: - Worklog
+
+class WorklogCreateView(CreateView):
+    model = Worklog
     fields = '__all__'
 
